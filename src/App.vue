60a2a1e31c65c5b310e8626070a47cb903b925f3
Fernando Martínez <46614852+zenitrems@users.noreply.github.com>
2022-11-16 12:59:27 -0500

    Darky on Dark

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
-<template>
-  <v-app id="app">
-    <v-app-bar dense dark>
-      <router-link to="/"><v-icon>home</v-icon></router-link> 
-      <router-link to="/map"><v-icon>map</v-icon></router-link> 
+  <template>
+    <v-app id="app">
+      <v-app-bar dense dark>
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>menu</v-icon>
+            </v-btn>
+          </template>
 
-      <v-spacer></v-spacer>
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <router-link to="/"><v-icon>home</v-icon></router-link> 
+        <router-link to="/map"><v-icon>map</v-icon></router-link> 
 
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
+        <v-spacer></v-spacer>
 
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-    <v-container fluid fill-height>
-      <v-slide-y-transition mode="out-in">
-        <router-view />
-      </v-slide-y-transition>
-    </v-container>
-  </v-app>
-</template>
+        
+      </v-app-bar>
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-container>
+    </v-app>
+  </template>
 
-<style lang="scss">
-#app {
-  background: #f5f5f5;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-}
-</style>
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      drawer: true,
-    };
-  },
-};
-</script>
+  <style>
+    #app {
+      background: #2d2d30;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    }
+  </style>
--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,159 +1,157 @@
-<template>
-  <v-container >
-    <v-layout justify-center>
-      <v-flex xs12 sm6 md4 height>
-        <v-autocomplete
-          v-model="searched"
-          :items="items"
-          :loading="isLoading"
-          :search-input.sync="search"
-          no-filter
-          item-text="name"
-          item-value="name"
-          label="busca un hotel"
-          prepend-icon="hotel"
-          return-object
-          cache-items
-          clearable
-        >
-        </v-autocomplete>
-      </v-flex>
-    </v-layout>
-    <v-layout align-center justify-center row fill-height>
-      <v-flex md5>
-        <v-card class="mx-auto" max-width="320">
-          <v-img
-            src="https://destinationlesstravel.com/wp-content/uploads/2022/05/Birds-eye-view-of-the-Cancun-Hotel-zone-on-a-beautiful-day.jpg.webp"
-            height="200px"
-          ></v-img>
+          <template>
+            <v-container fill-height id="wrapper">
+              <v-row align="center" justify="center">
+                <v-col>
+                  <v-container fluid fill-height>
+                    <v-autocomplete
+                    v-model="searched"
+                    :items="items"
+                    :loading="isLoading"
+                    :search-input.sync="search"
+                    no-filter
+                    item-text="name"
+                    item-value="name"
+                    label="busca un hotel"
+                    prepend-icon="hotel"
+                    return-object
+                    cache-items
+                    clearable
+                    dark
+                    >
+                  </v-autocomplete>
+                </v-container>
+                
+                <v-container fluid>
+                  <v-card class="mx-auto" max-width="500" dark >
+                    <v-img
+                    src="https://destinationlesstravel.com/wp-content/uploads/2022/05/Birds-eye-view-of-the-Cancun-Hotel-zone-on-a-beautiful-day.jpg.webp"
+                    height="200px"
+                    ></v-img>
+                    <v-card-title v-text="searched.name"></v-card-title>
+                    <v-card-subtitle v-text="searched.address"></v-card-subtitle>
+                    <v-card-actions>
+                      <v-btn
+                      color="orange lighten-2"
+                      text
+                      @click="hotelLoc(searched.geoCode)"
+                      >
+                      Explore
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn icon @click="show = !show">
+                      <v-icon>{{
+                        show ? "keyboard_arrow_up" : "keyboard_arrow_down"
+                      }}</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                  <v-expand-transition>
+                    <div v-show="show">
+                      <v-divider></v-divider>
+                      <v-card-text v-text="searched.geoCode"> </v-card-text>
+                    </div>
+                  </v-expand-transition>
+                </v-card>
+              </v-container>
 
-          <v-card-title v-text="searched.name"></v-card-title>
+              <v-container>
+                <v-card  class="mx-auto" max-width="500" dark>
+                  <!-- map -->
+                  <l-map
+                  class="mapis"
+                  style="height: 300px"
+                  :zoom="zoom"
+                  :center="center"
+                  >
+                  <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+                  <l-marker :lat-lng="center">
+                    <l-popup>
+                      <span v-text="searched.name"> </span>
+                      <v-spacer> </v-spacer>
+                      <span v-text="searched.id"></span>
+                    </l-popup>
+                  </l-marker>
+                </l-map>
+              </v-card>
+            </v-container>
+          </v-col>
+        </v-row>
 
-          <v-card-subtitle v-text="searched.address"></v-card-subtitle>
+      </v-container>
+    </template>
+    <style>
+      #wrapper {
+       /*background-color: #d1d1d1 ;*/
+     }
+     .mapFlex {
+      z-index: 0;
+    }
+    .mapis {
+      max-height: 360;
+    }
+  </style>
+  <script>
+    import axios from "axios";
+    import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
+    export default {
+      name: "MapView",
+      components: {
+        LMap,
+        LTileLayer,
+        LMarker,
+        LPopup,
+      },
+      data() {
+        return {
+          url: "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+          attribution:
+          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+          zoom: 15,
+          center: [21.115, -86.817],
 
-          <v-card-actions>
-            <v-btn
-              color="orange lighten-2"
-              text
-              @click="hotelLoc(searched.geoCode)"
-            >
-              Explore
-            </v-btn>
+          isLoading: false,
+          items: [],
+          searched: [],
+          search: null,
 
-            <v-spacer></v-spacer>
-
-            <v-btn icon @click="show = !show">
-              <v-icon>{{
-                show ? "keyboard_arrow_up" : "keyboard_arrow_down"
-              }}</v-icon>
-            </v-btn>
-          </v-card-actions>
-          <v-expand-transition>
-            <div v-show="show">
-              <v-divider></v-divider>
-
-              <v-card-text v-text="searched.geoCode"> </v-card-text>
-            </div>
-          </v-expand-transition>
-        </v-card>
-      </v-flex>
-      <v-flex md5 class="mapFlex">
-        <v-card>
-          <!-- map -->
-          <l-map
-            class="mapis"
-            style="height: 623px"
-            :zoom="zoom"
-            :center="center"
-          >
-            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-            <l-marker :lat-lng="center">
-              <l-popup>
-                <span v-text="searched.name"> </span>
-                <v-spacer> </v-spacer>
-                <span v-text="searched.id"></span>
-              </l-popup>
-            </l-marker>
-          </l-map>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</template>
-<style>
-.wrapper {
-  padding: 20px;
-}
-.mapFlex {
-  z-index: 0;
-}
-.mapis {
-  max-height: 400px;
-}
-</style>
-<script>
-import axios from "axios";
-import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
-export default {
-  name: "MapView",
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup,
-  },
-  data() {
-    return {
-      url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [21.115, -86.817],
-
-      isLoading: false,
-      items: [],
-      searched: [],
-      search: null,
-
-      hotels: [],
-      show: false,
+          hotels: [],
+          show: false,
+        };
+      },
+      watch: {
+        search(val) {
+                // Items have already been loaded
+          if (!val) {
+            return;
+          }
+          this.isLoading = true;
+                // Lazily load input items
+          this.searchDebounced(val);
+        },
+      },
+      methods: {
+              //debounce search query to avoid spamming the server
+        searchDebounced() {
+                // cancel pending call
+          clearTimeout(this._timerId);
+          this.isLoading = true;
+                //delay new call
+          this._timerId = setTimeout(() => {
+            axios
+            .get("/search", { params: { keyword: this.search } })
+            .then((res) => {
+              this.items = res.data.data;
+                      //console.log(this.items);
+              this.isLoading = false;
+            })
+            .catch((err) => {
+              console.log(err);
+              this.isLoading = false;
+            });
+          }, 850);
+        },
+        hotelLoc(geoCode) {
+          this.center = [geoCode.latitude, geoCode.longitude];
+        },
+      },
     };
-  },
-  watch: {
-    search(val) {
-      // Items have already been loaded
-      if (!val) {
-        return;
-      }
-      this.isLoading = true;
-      // Lazily load input items
-      this.searchDebounced(val);
-    },
-  },
-  methods: {
-    //debounce search query to avoid spamming the server
-    searchDebounced() {
-      // cancel pending call
-      clearTimeout(this._timerId);
-      this.isLoading = true;
-      //delay new call
-      this._timerId = setTimeout(() => {
-        axios
-          .get("/search", { params: { keyword: this.search } })
-          .then((res) => {
-            this.items = res.data.data;
-            //console.log(this.items);
-            this.isLoading = false;
-          })
-          .catch((err) => {
-            console.log(err);
-            this.isLoading = false;
-          });
-      }, 850);
-    },
-    hotelLoc(geoCode) {
-      this.center = [geoCode.latitude, geoCode.longitude];
-    },
-  },
-};
-</script>
+  </script>
