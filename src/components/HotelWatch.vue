af6a231df0ea29ab802519ab8b5cb21ff93db35f
Fernando Martínez <46614852+zenitrems@users.noreply.github.com>
2022-11-28 12:43:37 -0500

    Change on the go, Iterate and show offers array

--- src/components/HotelWatch.vue
+++ src/components/HotelWatch.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container class="center-padding" fluid fill-height>
-    <v-row align="center">
-      <v-col cols="12">
-        <v-card class="mx-auto" max-width="600">
+    <v-row>
+      <v-col>
+        <v-card class="mx-auto" max-width="600" elevation="12">
           <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
             <v-card-title>{{ item.hotel.name }}</v-card-title>
           </v-img>
@@ -10,27 +10,56 @@
               item.hotel.rating
           }} Stars</v-card-subtitle>
           <v-card-text class="text--primary">
-            Description
             <div>
               {{ hotelDescription.text }}
             </div>
           </v-card-text>
           <v-card-text>
             Address
-            <div>{{ hotelAddress }}</div>
+            <div>{{ hotelAddress.lines }} {{ hotelAddress.cityName }} {{ hotelAddress.stateCode }},
+              {{ hotelAddress.postalCode }} {{ hotelAddress.countryCode }}</div>
           </v-card-text>
           <v-card-text>
             Amenities
             <div>{{ hotelAmenities }}</div>
           </v-card-text>
-          <v-card-text>
-            Offers
-            <div>{{ hotelOffers }}</div>
-          </v-card-text>
           <v-card-actions>
             <v-btn color="orange" text> Share </v-btn>
             <v-btn color="orange" text> Explore </v-btn>
           </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" v-for="hotelOffer in hotelOffers" :key="hotelOffer.id">
+        <v-card class="mx-auto" max-width="600" elevation="12">
+          <v-card-title>
+            <div>
+              {{ hotelOffer.price.base }} {{ hotelOffer.price.currency }}
+            </div>
+            <v-spacer></v-spacer>
+            <div>
+              Check In: {{ hotelOffer.checkInDate }}
+            </div>
+            <v-spacer></v-spacer>
+            <div>
+              Check Out: {{ hotelOffer.checkOutDate }}
+            </div>
+          </v-card-title>
+          <v-card-subtitle>
+            <div>
+              Habitación: {{ hotelOffer.room.description.text }}
+            </div>
+          </v-card-subtitle>
+          <v-card-text>
+            <div>
+              Tipo de Cama: {{ hotelOffer.room.typeEstimated.bedType }}
+            </div>
+            <div>
+              Numero de camas: {{ hotelOffer.room.typeEstimated.beds }}
+            </div>
+            <div>
+              Adultos: {{ hotelOffer.guests.adults }}
+            </div>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -42,6 +71,7 @@
 }
 </style>
 <script>
+
 import axios from "axios";
 export default {
   data() {
@@ -66,19 +96,24 @@ export default {
           this.hotelDescription = res.data.data.hotel.description;
           this.hotelAddress = res.data.data.hotel.address;
           this.hotelAmenities = res.data.data.hotel.amenities;
-          this.hotelMedia = res.data.data.hotel.media;
-
           this.hotelOffers = res.data.data.offers;
+          this.hotelMedia = res.data.data.hotel.media;      
 
-          this.body = res.data.body;
+          /* this.body = res.data.body; */
+          /* console.log(this.body); */
 
+          //status codes
           this.statusText = res.statusText;
           this.statusCode = res.status;
 
           console.log(this.item);
           console.log(this.hotelMedia);
+          console.log(this.hotelAmenities);
+          console.log(this.hotelOffers);
+          console.log(this.hotelAddress);
 
-          console.log(this.body);
+
+
           console.log(this.statusText, this.statusCode);
         })
         .catch((err) => {
